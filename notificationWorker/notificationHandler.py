19048309bdd4849c08f3b80024a4a09d51d9ecbd
narberal <nicdanyos@gmail.com>
2020-07-02 14:37:10 +0700

    Add documentation for next development

--- notificationWorker/notificationHandler.py
+++ notificationWorker/notificationHandler.py
@@ -1,10 +1,18 @@
 import natsPublish
 
-# fetch latest timestamp from database
+# call stored procedure from DB to fetch input data for prediction
+## def getPredictionInputData() // can be run as separate service / API, py file stored on predictionWorker
+
+# call predict function, store prediction result on DB
+## def predictData(): // can be run as separate service / API, py file stored on predictionWorker
+
+# fetch latest timestamp from database to show latest prediction data
 def getTimeStamp():
     timestamp = 23000999
     data = {'timestamp': timestamp}
     return data
 
 # run worker once:
+## getPredictionInputData() // 
+## predictData()
 natsPublish.runPublisher(getTimeStamp())
